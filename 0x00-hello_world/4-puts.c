39f435a4e08cafc48afc02bcb6a744f65d300150
Harry Onah <onahharry@gmail.com>
2023-02-16 12:19:28 -0800

    update on puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main block
- * return 0
+ * Main - main block
+ * Return: 0
+ * Description: this program outputs a string without printf
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
