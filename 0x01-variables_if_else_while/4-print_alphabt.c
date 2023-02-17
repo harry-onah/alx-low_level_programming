507c75f5bcc8427e48fc33a2b8e4a6ed98494b42
Harry Onah <onahharry@gmail.com>
2023-02-17 13:02:24 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ * Description: lowercase alphabets except q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
