341f4ad57aec13af7a5413bc4d5387b739d758fb
Harry Onah <onahharry@gmail.com>
2023-02-17 14:38:58 -0800

    100-print_comb3.c update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,10 +7,11 @@
  * combinations of two digits.
  * Numbers must be separated by ,, followed by a space
  * The two digits must be different
- * 01 and 10 are considered the same combination of the 
+ * 01 and 10 are considered the same combination of the
  * two digits 0 and 1
  * Print only the smallest combination of two digits
  * Numbers should be printed in ascending order, with two digits
+ * Return: 0
  */
 int main(void)
 {
@@ -41,4 +42,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
