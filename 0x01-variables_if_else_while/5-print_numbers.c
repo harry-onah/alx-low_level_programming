7bff2ad954ce996829c70f548143808f5c97fe32
Harry Onah <onahharry@gmail.com>
2023-02-17 13:13:56 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ * Description: a program to print the numbers 0 to 9 in bas 10
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+
+	return 0;
+}
