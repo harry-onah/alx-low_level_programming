1326e7dd076dc8c64e22c95dcdf39023ba66a664
Harry Onah <onahharry@gmail.com>
2023-02-21 12:57:27 -0800

    8-24_hours.c update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,32 +14,34 @@ void jack_bauer(void)
 
 	h2 = 0;
 	while (h2 <= 2)
+	{
 		if (h2 == 2)
 		{
 			/* Restrain to 23h, not 29 */
 			a = 3;
 		}
 		
-	h1 = 0;
-	while ( h1 <= a)
-	{
-		m2 = 0;
-		while (m2 <= 5)
+		h1 = 0;
+		while ( h1 <= a)
 		{
-			m1 = 0;
-			while (m1 <= 9)
+			m2 = 0;
+			while (m2 <= 5)
 			{
-				_putchar('0' + h2);
-				_putchar('0' + h2);
-				_putchar(':');
-				_putchar('0' + m2);
-				_putchar('0' + m1);
-				_putchar('\n');
-				m1++;
+				m1 = 0;
+				while (m1 <= 9)
+				{
+					_putchar('0' + h2);
+					_putchar('0' + h2);
+					_putchar(':');
+					_putchar('0' + m2);
+					_putchar('0' + m1);
+					_putchar('\n');
+					m1++;
+				}
+				m2++;
 			}
-			m2++;
+			h1++;
 		}
-		h1++;
-	}
-	h2++;
+		h2++;
+ 	}
 }
