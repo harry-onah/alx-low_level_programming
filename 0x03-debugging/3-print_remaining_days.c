257508c2695acb2221fe4ba3a8f79595d06fa00d
Harry Onah <onahharry@gmail.com>
2023-02-22 10:38:08 -0800

    print remaining days correction

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -29,10 +29,5 @@ void print_remaining_days(int month, int day, int year)
 	    printf("Day of the year: %d\n", day);
             printf("Remaining days: %d\n", 365 - day);
         }
-        else
-        {
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
     }
 }
