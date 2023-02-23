6aa617164b3de09626cfdb6bf1a95375210425eb
Harry Onah <onahharry@gmail.com>
2023-02-23 09:21:15 -0800

    10-print_triangles and others

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - function to print number
+ * @n: integer
+ * Return: always
+ */
+void print_number(int n)
+{
+	umsigned int num = n;
+	
+	if (n < 0)
+	{
+		_putchar('_');
+		num = -num;
+	}
+	if((num / 10) > 0)
+	{
+		print_number(num / 10)
+	}
+	_putchar((num % 10) + '0');
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints 10 times the numbers
+ * from 0 to 14
+ * Return: 0-14 10times
+ */
+void more_numbers(void)
+{
+	int i, row;
+	
+	for (row = 0; row <= 9; row++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
