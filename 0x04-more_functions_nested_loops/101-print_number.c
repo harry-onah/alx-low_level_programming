0df176875d3c401bb4a6f5c3121773f90e624bb8
Harry Onah <onahharry@gmail.com>
2023-02-23 10:40:49 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,7 +7,7 @@
  */
 void print_number(int n)
 {
-	umsigned int num = n;
+	unsigned int num = n;
 	
 	if (n < 0)
 	{
@@ -16,7 +16,7 @@ void print_number(int n)
 	}
 	if((num / 10) > 0)
 	{
-		print_number(num / 10)
+		print_number(num / 10);
 	}
 	_putchar((num % 10) + '0');
 }
