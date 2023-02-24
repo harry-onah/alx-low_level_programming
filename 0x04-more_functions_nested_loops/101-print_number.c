e52bfe598602913bc808e14e267c263a69bb8c4f
Harry Onah <onahharry@gmail.com>
2023-02-24 01:52:05 -0800

    101-print_number.c changes

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,7 +8,6 @@
 void print_number(int n)
 {
 	unsigned int num = n;
-	
 	if (n < 0)
 	{
 		_putchar('-');
@@ -18,6 +17,5 @@ void print_number(int n)
 	{
 		print_number(num / 10);
 	}
-	
 	_putchar((num % 10) + '0');
 }
