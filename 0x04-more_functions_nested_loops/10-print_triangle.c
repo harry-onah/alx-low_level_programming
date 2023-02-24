4b007f56acc8afc48c0b62f20593ae56c9a03427
Harry Onah <onahharry@gmail.com>
2023-02-23 22:44:39 -0800

    10-print_triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -19,7 +19,7 @@ void print_triangle(int size)
 	{
 		for (ro = 0; ro <= (size -1); ro++)
 		{
-			for (sp = 0; sp < (size - 1) - ro; sp++)
+			for (sp = 0; sp < ((size - 1) - ro); sp++)
 			{
 				_putchar(' ');
 			}
