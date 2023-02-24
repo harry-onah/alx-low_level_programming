f37029ef040dde22f28f4c68a714d396d5fed93b
Harry Onah <onahharry@gmail.com>
2023-02-23 22:50:25 -0800

    10-print_triangle.c corrections

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,7 +17,7 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (ro = 0; ro <= (size -1); ro++)
+		for (ro = 0; ro <= (size - 1); ro++)
 		{
 			for (sp = 0; sp < ((size - 1) - ro); sp++)
 			{
