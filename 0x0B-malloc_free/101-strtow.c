59e7414420f55953aa34def4a8e7926f2ad59cde
Harry Onah <onahharry@gmail.com>
2023-03-14 19:26:59 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -44,7 +44,7 @@ char **strtow(char *str)
 	words = count_word(str);
 	if (words == 0)
 		return (NULL);
-	matrix = (char **) malloc(sizeof(char *) * (words + 1))
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 		if (matrix == NULL)
 			return (NULL);
 	for (i = 0; i <= len; i++)
