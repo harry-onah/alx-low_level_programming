08d40ecc12b2a218f1758dd257002160dcf6991b
Harry Onah <onahharry@gmail.com>
2023-03-15 03:01:45 -0700

    changed to _putchar.c

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,13 +1,17 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _puts - prints a string followed by a new line to stdout
+ * @str: string to be printed
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: nothing on success
  */
-int _putchar(char c)
+void _puts(char *str)
 {
-	return (write(1, &c, 1));
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
