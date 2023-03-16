faa00568dbaf4f507d667901423c19f1e17573e9
Harry Onah <onahharry@gmail.com>
2023-03-16 10:27:08 -0700

    more malloc task corrections

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,6 +17,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	else
 		return (malloc(new_size));
 
+	if (new_size == old_size)
+		return (ptr);
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
@@ -26,7 +29,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (relloc == NULL)
 		return (0);
 
-	for (i = 0; (i < old_size || i < new_size); i++)
+	for (i = 0; i < (old_size || i < new_size); i++)
 		*(relloc + i) = clone[i];
 
 	free(ptr);
