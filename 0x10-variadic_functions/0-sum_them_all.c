ffba8a6be44d6b65b3ca8303aaf24f7162e012c4
Harry Onah <onahharry@gmail.com>
2023-03-28 13:30:21 +0000

    0x10-variadic_functions task solutions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - prints the sum of all its parameters
+ * @n: integer number
+ * @...: A variable number of integer to be printed
+ *
+ * Return: sum, 0 otherwise
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list pa;
+	unsigned int i;
+	int sum;
+
+	if (n == 0)
+		return (0);
+
+	va_start(pa, n);
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(pa, int);
+
+	va_end(pa);
+	return (sum);
+}
